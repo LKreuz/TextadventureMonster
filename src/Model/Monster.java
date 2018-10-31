998cf9325574cedea94020dc7f046cbbc42b9902
sinemz <sinemz-01@outlook.com>
2018-10-31 12:57:23 +0100

    Kommentare zu Model

--- src/Model/Monster.java
+++ src/Model/Monster.java
@@ -1,8 +1,11 @@
 package Model;
 
 public class Monster extends Figure {
+
+//Attribut
     private boolean loaded;
 
+//Konstruktor
     public Monster(int Strength) {
         super(Strength);
         loaded = false;
@@ -10,6 +13,7 @@ public class Monster extends Figure {
 
     }
 
+//Array für abwechslungsreiche Monsternamemn
     public String setMonstername() {
         String[] MN = new String[10];
         MN[0] = "Poisongolem";
@@ -27,6 +31,7 @@ public class Monster extends Figure {
         return MN[index];
     }
 
+//Methode sucht zufällige Action für Monster aus
     public int getRandomNumber(){
         int randomAction = (int) (Math.random() * 2);
         return randomAction;
@@ -61,7 +66,7 @@ public class Monster extends Figure {
         return new String ("action fehlgeschlagen");
     }
 
-
+//Polymorphie abgänderte Mthodeo aus der Oberklasse
     public String lose(){
             return "Monster lost! You win and save the princess";
 
